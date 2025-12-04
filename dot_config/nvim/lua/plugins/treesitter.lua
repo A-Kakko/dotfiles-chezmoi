182ce99b6355a1cee94c063f630ea530ddc48124
A-Kakko <kkbjdynasty@gmail.com>
2025-12-05 00:57:28 +0900

    自動デデント

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,18 +1,22 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter-textobjects",
-		dependencies = "nvim-treesitter/nvim-treesitter",
-		opts = {
-			textobjects = {
-				select = {
-					enable = true,
-					lookahead = true,
-					keymaps = {
-						["af"] = "@function.outer",
-						["if"] = "@function.inner",
-					},
-				},
-			},
-		},
-	},
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    opts = {
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+          },
+        },
+      },
+    },
+  },
+  {
+    "Vimjas/vim-python-pep8-indent",
+    ft = "python",
+  },
 }
